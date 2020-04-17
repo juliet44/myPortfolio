6e33084490a040daa232fe38eb75b69ba7b89522
juliet44 <julietnjeri144@gmail.com>
2020-04-17 05:47:52 +0300

    first commit

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,7 +1,32 @@
+
+
+
+$(document).ready(function() {
+ 
+   $(".navbar a").on('click', function(event) {     
+    
+     if (this.hash !== "") {
+       
+       event.preventDefault();
+      
+       const hash = this.hash;
+       
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top - 60
+       }, 800
+       , function() {        
+         window.location.hash = hash;
+       }
+       );
+     } // End if
+   });
+ });
+
+//resume tabs
 //btns
 let educ=document.querySelector("#educ");
 let exp=document.querySelector("#exp");
-let skill=document.querySelector("#skill");
+let project=document.querySelector("#project");
 
 //all
 let tabContent=document.querySelectorAll(".tabContent");
@@ -18,7 +43,7 @@ exp.addEventListener("click", ()=>{
    showPanel(1);
 
 });
-skill.addEventListener("click", ()=>{
+project.addEventListener("click", ()=>{
    showPanel(2);
 });
 
@@ -38,6 +63,44 @@ skill.addEventListener("click", ()=>{
  }
  
 
- showPanel(2);
+ showPanel(0);
+
+//carousel interval
+ $('.carousel').carousel({
+   interval: 3000
+})
 
 
+///animations 
+const headings= document.querySelectorAll(".head");
+console.log(headings);
+const observer= new IntersectionObserver(function(entries){
+
+  entries.forEach(entry=>{
+    if(entry.intersectionRatio >0){
+      entry.target.style.animation = `anim1 2s forwards ease-out`;
+    }else{
+      entry.target.style.animation='none'; 
+    }
+  });  
+});
+
+headings.forEach(heading=>{
+  observer.observe(heading);
+});
+
+//about content
+const aboutContent= document.querySelector(".aboutContent");
+
+const observerContent= new IntersectionObserver(function(entries){
+
+  entries.forEach(entry=>{
+    if(entry.intersectionRatio >0){
+      entry.target.style.animation = `anim2 3s forwards ease-out`;
+    }else{
+      entry.target.style.animation='none'; 
+    }
+  });  
+});
+
+observerContent.observe(aboutContent);
