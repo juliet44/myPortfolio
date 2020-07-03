98e4c23fed763368cb2433d01eb809bfbd83e163
juliet44 <julietnjeri144@gmail.com>
2020-07-03 06:37:17 +0300

    first commit

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,3 +1,66 @@
+<?php
+
+$msg = "";
+$msgClass = '';
+$error = "";
+if (filter_has_var(INPUT_POST, 'submit')) {
+
+  //get form data
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $subject = htmlspecialchars($_POST['subject']);
+  $message = htmlspecialchars($_POST['message']);
+
+
+
+
+  if (!empty($name) && !empty($email) && !empty($subject) && !empty($message)) {
+    //passed   
+    //check email 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+      //failed
+      $msg = "Please use a valid Email!";
+      $msgClass = "alert-danger";
+    } else {
+      //passed
+      $toEmail = 'julietgachoki44@gmail.com';
+      $subject = 'REF:' . $subject;
+      $body = '<h2>Contact Request</h2>
+              <h4>Name</h4><p>' . $name . '</p>
+              <h4>Email</h4><p>' . $email . '</p>                           
+              <h4>Message</h4><p>' . $message . '</p>
+            ';
+
+      //headers
+
+      // Email Headers
+      $headers = "MIME-Version: 1.0" . "\r\n";
+      $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+
+      // Additional Headers
+      $headers .= "From: " . $name . "<" . $email . ">" . "\r\n";
+
+      if (mail($toEmail, $subject, $body, $headers)) {
+        // Email Sent
+        $msg = 'Your email has been sent';
+        $msgClass = 'alert-success';
+        $name = $email = $subject = $message = "";
+      } else {
+        // Failed
+        $msg = 'Your email was not sent';
+        $msgClass = 'alert-danger';
+      }
+    }
+  } else {
+    //failed
+    $error = "This field is required!";
+  }
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,20 +70,24 @@
   <title>Juliet Njeri</title>
   <link rel="stylesheet" href="bootstrap\css\bootstrap.min.css">
   <link rel="stylesheet" href="css/all.min.css">
-  <link rel="stylesheet" href="app.css">
-  <link rel="stylesheet" href="carousel.css">
-  <link rel="stylesheet" href="form.css">
+  <link rel="stylesheet" href="app.css?v=version2">
+  <link rel="stylesheet" href="slider.css">
+  <link rel="stylesheet" href="contact.css">
   <link rel="stylesheet" href="animation.css">
+  <link rel="icon" href="images/icon.png">
 </head>
 
 
 <body>
+
+  <div class="loader-wrapper">
+    <span class="loader"><span class="loader-inner"></span></span>
+  </div>
   <!-- the navabr is here -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
       <a id="brand" class="navbar-brand" href="#home"><span>J</span>uliet<span>.</span></a>
-      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTop"
-        aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTop" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="icon-bar top-bar"></span>
         <span class="icon-bar middle-bar"></span>
         <span class="icon-bar bottom-bar"></span>
@@ -54,7 +121,7 @@
     <div id="intro" class="container">
       <div class="row" class="col-sm-12">
         <div class="col-m-6">
-          <img src="images/myselftwo.jpg" width="150px" height="200px" class="img-fluid">
+          <img src="images/myselftwo.jpg" width="150px" height="200px" alt="my profile" class="img-fluid">
 
         </div>
         <div class="col-6">
@@ -74,18 +141,14 @@
   <!-- about us page -->
 
   <div id="about" class="hcontact padding">
-    <div class="container row bg-white">
+    <div class="container-fluid row bg-white">
       <div class="col-sm-12">
         <h1 class="head">About <span> Me.</span></h1>
         <div class="aboutContent">
-          <h5 class="m-4"><b> My name is Juliet Njoki Njeri And I am The Newbie Newbiest in Web Development.</b></h5>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam perferendis sint exercitationem
-            voluptatum
-            quisquam a quibusdam neque harum dolorem, odio, repellendus esse? Itaque omnis hic ad fugit assumenda
-            quisquam
-            libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos numquam deserunt molestias
-            excepturi, quos totam cum aperiam commodi quo, et, impedit rerum ipsam. Esse laboriosam qui dolorum
-            voluptatibus iusto dignissimos!</p>
+          <h4 class="m-4"><b>Hello! Am Juliet, A Web Developer.</b></h4>
+          <p>My name is Juliet Njeri and most of my friends prefer to call me Juliet. I am web developer focused on crafting great web experiences. Designing and Coding has been my passion since the days I started working with computers.
+            I enjoy creating beautifully designed, intuitive and functional websites. One of my dreams is to become a full-stack Developer and inspire other women in tech to break the barriers in tech.</p>
+          <p>Want to know more about me? You can find me on <a href="https://www.linkedin.com/in/juliet-njeri-379475130/">Linkedin,</a> </p>
 
           <div id="icons" class="row mt-4">
             <div class=" col-sm mt-3">
@@ -119,7 +182,7 @@
           <div class="tab">
             <button id="educ"><i class="fas fa-graduation-cap"></i> Education</button><br>
             <button id="exp"><i class="fas fa-briefcase"></i> Experience</button><br>
-            <button id="project"><i class="fas fa-th-list"></i> Projects</button>
+            <button id="project"><i class="fas fa-th-list"></i> Skills</button>
           </div>
         </div>
 
@@ -128,48 +191,42 @@
           <div id="Education" class="tabContent">
             <h2>- Bachelor of Science in Computer Science</h2>
             <h5><i>Karatina University - 2019</i></h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,optio. Dolorum adipisci, sit voluptas
-              amet
-              animi sapiente. Neque fuga nesciunt cum veritatis expedita,repellat, quo aspernatur, facilis quia
-              accusantium tempore!</p>
+            <p>Majored in software development. Successfully completed and graduated with a second class Honors Upper division. </p>
 
 
             <h2>- Routing and Switching</h2>
             <h5><i>Huawei Academy - 2019</i> </h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,optio. Dolorum adipisci, sit voluptas
-              amet
-              animi sapiente. Neque fuga nesciunt cum veritatis expedita,repellat, quo aspernatur, facilis quia
-              accusantium tempore!</p>
+            <p>Gained foundational networking knowledge and the ability to install, configure, operate, and troubleshoot medium-size routed and switched networks.</p>
 
             <h2>- Mobile and Web Development</h2>
             <h5><i>Emobilis College - 2018</i> </h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,optio. Dolorum adipisci, sit voluptas
-              amet
-              animi sapiente. Neque fuga nesciunt cum veritatis expedita,repellat, quo aspernatur, facilis quia
-              accusantium tempore!</p>
+            <p> Studied on how to create content and software for mobile handsets using technologies including JAVA & Android, PHP & MySQL, HTML5 and Bootstrap framework.</p>
 
-              <h1>education</h1>
+            <h1>education</h1>
           </div>
 
           <div id="Experience" class="tabContent">
             <h2>- Zukabit Company Limited</h2>
             <h5><i>Graduate Trainee - 2020</i> </h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,optio. Dolorum adipisci, sit voluptas
-              amet
-              animi sapiente. Neque fuga nesciunt cum veritatis expedita,repellat, quo aspernatur, facilis quia
-              accusantium tempore!</p>
-
+            <ul>
+              <li>Intensive training in web design technologies through the guidance of the Lead Developer accompanied with development. </li>
+              <li>Front-End programming using HTML, CSS and JavaScript. </li>
+              <li>PHP and MySql programming.</li>
+            </ul>
             <h2>- Nation Media Group</h2>
             <h5><i> Technical Support Intern - 2018</i> </h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,optio. Dolorum adipisci, sit voluptas
-              amet
-              animi sapiente. Neque fuga nesciunt cum veritatis expedita,repellat, quo aspernatur, facilis quia
-              accusantium tempore!</p>
+            <ul>
+              <li>1st Level technical support at the Service Desk to users.</li>
+              <li>Administration of Mail Monitoring Software.</li>
+              <li>Installation and configuration of end-user operationg systems and applications.</li>
+              <li> Setting up new PC and printer hardware as per NMG configurations.</li>
+              <li>Basic network support and troubleshooting.</li>
+            </ul>
 
-              <h1>experience</h1>
+            <h1>experience</h1>
           </div>
 
-          
+
 
           <div id="project " class="tabContent">
 
@@ -177,34 +234,34 @@
             <div class="row">
               <div class="col-sm-4">
                 <img src="images/html.png" width="100px" height="100px">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?</p>
+                <p>Describe the contents and appearance of Web pages.</p>
               </div>
               <div class="col-sm-4">
                 <img src="images/css.png" width="70px" height="100px">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?</p>
+                <p>Describe the presentation of Web pages, including colors, layout, and fonts.</p>
               </div>
-               <div class="col-sm-4">
+              <div class="col-sm-4">
                 <img src="images/js.png" width="90px" height="100px">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?</p>
+                <p>Create interactive websites that bring great user experience using JS Frameworks.</p>
               </div>
             </div>
             <div class="row">
               <div class="col-sm-4">
                 <img src="images/php.png" width="100px" height="100px">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?</p>
+                <p>Ability to manage dynamic content, databases and session tracking.</p>
               </div>
               <div class="col-sm-4">
                 <img src="images/sql.png" width="100px" height="100px">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?</p>
+                <p>Perform update data on a database, or retrieve data from a database among other tasks.</p>
               </div>
 
               <div class="col-sm-4">
                 <img src="images/press.png" width="100px" height="100px">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?</p>
+                <p>Ability to create flawless and functional wordpress websites.</p>
               </div>
             </div>
 
-              <h1>my skills</h1>
+            <h1>my skills</h1>
           </div>
         </div>
       </div>
@@ -224,23 +281,22 @@
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="image-area">
-              <img src="images/test.jpg" alt="...">
+              <img src="images/nmg.png" alt="Nation Media Group">
             </div>
             <div class="carousel-caption">
-              <h3>Mercy Kimani</h3>
+              <h3>HR</h3>
               <h4>Nation Media Group</h4>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam corrupti, molestiae architecto repellat magni vel impedit repellendus
-                expedita enim sit fugit porro, libero voluptatum cupiditate debitis veritatis non, nesciunt corporis.
-              </p>
+              <p>For the period that she was with us, she was a dedicated intern with good discipline, teamwork, focus, punctuality
+                and also a good sense of decent dressing.Based on her performance, dedication and willingness to learn, I would strongly
+                recommend her to any IT support role. </p>
             </div>
           </div>
           <div class="carousel-item">
             <div class="image-area">
-              <img src="images/test.jpg" alt="...">
+              <img src="images/karu.png" alt="Karatina University">
             </div>
             <div class="carousel-caption">
-              <h3>M. Kariuki</h3>
+              <h3>M. K</h3>
               <h4>Karatina University</h4>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur
                 adipisicing elit. Hic doloribus nam quia adipisci vel quisquam quos quod! Dolorem unde, ad veritatis
@@ -282,97 +338,105 @@
       </div>
 
       <div class="row">
-       
         <div class="col-md-7">
+          <?php if ($msg != '') : ?>
+            <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+          <?php endif; ?>
           <div class="message">
             <div class="titleform">
               <h3 class="hcontact">Send a message</h3>
             </div>
-
-            <div id="successMessage">
-
-            </div>
-            <form action="" method="" id="contactForm" class="justify-content-center">
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactForm" class="justify-content-center">
               <div class="row">
-                <div class="form-group col">
-                  <input class="form-control" type="text" id="name" placeholder="Enter your name">
-                  <div class="error" id="namErr"></div>
+                <div class="form-group col-2">
+                  <label for="name">Name:</label>
+                </div>
+                <div class="form-group col-10">
+                  <input class="form-control" type="text" id="name" name="name" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+                  <div class="error" id="namErr"><?php echo $error; ?></div>
                 </div>
               </div>
+
               <div class="row">
-                <div class="form-group col">
-                  <input class="form-control" type="email" id="email" placeholder="Enter a valid Email Address">
-                  <div class="error" id="emailErr"></div>
+                <div class="form-group col-2">
+                  <label for="email">Email:</label>
                 </div>
-
-                <div class="form-group col">
-                  <input class="form-control" type="text" id="phoneNumber" placeholder="Mobile (optional)">
-                  <div class="error" id="mobileErr"></div>
+                <div class="form-group col-10">
+                  <input class="form-control" type="email" id="email" name="email" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+                  <div class="error" id="emailErr"><?php echo $error; ?></div>
                 </div>
               </div>
-              <div class="form-group">
-                <input class="form-control" type="text" id="subject" placeholder="Subject">
-                <div class="error" id="subErr"></div>
+
+              <div class="row">
+                <div class="form-group col-2">
+                  <label for="subject">Subject:</label>
+                </div>
+                <div class="form-group col-10">
+                  <input class="form-control" type="text" id="subject" name="subject" value="<?php echo isset($_POST['subject']) ? $subject : ''; ?>">
+                  <div class="error" id="subErr"><?php echo $error; ?></div>
+                </div>
               </div>
 
-              <div class="form-group">
-                <textarea class="form-control" id="message" rows="3" placeholder="Message"></textarea>
-                <div class="error" id="messagErr"></div>
+              <div class="row">
+                <div class="form-group col-2">
+                  <label for="subject">Message:</label>
+                </div>
+                <div class="form-group col-10">
+                  <textarea class="form-control" id="message" rows="3" name="message"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+                  <div class="error" id="messagErr"><?php echo $error; ?></div>
+                </div>
               </div>
-              <button type="submit" id="send" class="button">SEND</button>
+
+              <button type="submit" id="send" name="submit" class="button">SEND</button>
             </form>
           </div>
         </div>
 
         <div class="col-md-5">
           <div id="contactDetails" class="slide-in from-right">
-              
-                <h4>Email: julietgachoki44@gmail.com</h4>
-                <form>
-                  <button><a href="images/Juliet_Njeri_CV.pdf" download="Juliet_Njeri_CV">Download CV</a></button>
-                </form>
+
+            <h4>Email: info@julietnjeri.com</h4>
+            <form>
+              <button><a href="images/Juliet_Njeri_CV.pdf" download="Juliet_Njeri_CV">Download CV</a></button>
+            </form>
           </div>
-    </div>
+        </div>
 
       </div>
     </div>
   </div>
 
- 
+
   <!-- addd our footer -->
-  <footer id="footer"> 
-   
-        <div class="container">
-     
-          <a href="https://www.linkedin.com/in/juliet-njeri-379475130/"><i class="fab fa-linkedin fa-lg"></i></a>
-          <a href="https://github.com/juliet44"><i class="fab fa-git-square fa-lg"></i></a>
-          <a><i class="fas fa-globe fa-lg"></i></i></a>
-       
-       
-       <p> &copy;Copyright 2020</p>
-      </div>
-      
+  <footer id="footer">
+
+    <div class="container">
+
+      <a href="https://www.linkedin.com/in/juliet-njeri-379475130/"><i class="fab fa-linkedin fa-lg"></i></a>
+      <a href="https://github.com/juliet44"><i class="fab fa-git-square fa-lg"></i></a>
+      <a><i class="fas fa-globe fa-lg"></i></i></a>
+
+
+      <p> &copy;Copyright 2020</p>
+    </div>
+
   </footer>
 
 
 
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
-  <script src="app.js">
+  <script src="text.js">
 
 
   </script>
 
 
-<a href="#home" class="back-to-top"></a>
+  <a href="#home" class="back-to-top"></a>
 
 </body>
 
--- portfolio/text.js
+++ portfolio/text.js
@@ -1,50 +1,46 @@
+$(window).on("load", function () {
+  $(".loader-wrapper").fadeOut("slow");
+});
 //scroll to top
-$(document).ready(function(){
- 
+$(document).ready(function () {
   //Check to see if the window is top if not then display button
-  $(window).scroll(function(){
- 
-   // Show button after 100px
-   var showAfter = 100;
-   if ( $(this).scrollTop() > showAfter ) { 
-    $('.back-to-top').fadeIn();
-   } else { 
-    $('.back-to-top').fadeOut();
-   }
+  $(window).scroll(function () {
+    // Show button after 100px
+    var showAfter = 100;
+    if ($(this).scrollTop() > showAfter) {
+      $(".back-to-top").fadeIn();
+    } else {
+      $(".back-to-top").fadeOut();
+    }
   });
-  
-  //Click event to scroll to top
-  $('.back-to-top').click(function(){
-   $('html, body').animate({scrollTop : 0},800);
-   return false;
-  });
-  
- });
 
+  //Click event to scroll to top
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+});
 
 //scroll to botttom
 $(document).ready(function () {
-
-  $(".navbar a").on('click', function (event) {
-
+  $(".navbar a").on("click", function (event) {
     if (this.hash !== "") {
-
       event.preventDefault();
 
       const hash = this.hash;
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - 60
-      }, 800
-        , function () {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 60,
+        },
+        800,
+        function () {
           window.location.hash = hash;
         }
       );
     } // End if
   });
 });
-
-
 
 //resume tabs
 //btns
@@ -57,20 +53,16 @@ let tabContent = document.querySelectorAll(".tabContent");
 let buttons = document.querySelectorAll(".tab button");
 //parent tabs
 
-
 educ.addEventListener("click", () => {
   showPanel(0);
-
 });
 
 exp.addEventListener("click", () => {
   showPanel(1);
-
 });
 project.addEventListener("click", () => {
   showPanel(2);
 });
-
 
 //tabs function
 function showPanel(panelIndex) {
@@ -79,95 +71,79 @@ function showPanel(panelIndex) {
   });
   buttons.forEach(function (b) {
     b.style.backgroundColor = "";
-
   });
 
   tabContent[panelIndex].style.display = "block";
   buttons[panelIndex].style.backgroundColor = "#ff8c00";
 }
 
-
-showPanel(2);
+showPanel(0);
 
 //carousel interval
-$('.carousel').carousel({
-  interval: 3000
-})
+$(".carousel").carousel({
+  interval: 3000,
+});
 
-
-///animations 
+///animations
 const headings = document.querySelectorAll(".head");
 const aboutContent = document.querySelector(".aboutContent");
-const slideIn=document.querySelector(".slide-in");
+const slideIn = document.querySelector(".slide-in");
 
-const appearOptions={
-root:null,
-threshold:0,
-margin:"0px"
+const appearOptions = {
+  root: null,
+  threshold: 0,
+  margin: "0px",
 };
 
-const appearOnscroll=new IntersectionObserver(function(entries, appearOnscroll){
-  entries.forEach(entry => {
-      if(entry.intersectionRatio >0){
-       entry.target.classList.add("appear");
-      }else{
-        
-        entry.target.classList.remove("appear");
-      }
-  }); 
-},appearOptions);
+const appearOnscroll = new IntersectionObserver(function (
+  entries,
+  appearOnscroll
+) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add("appear");
+    } else {
+      entry.target.classList.remove("appear");
+    }
+  });
+},
+appearOptions);
 
 appearOnscroll.observe(slideIn);
 
-
-
-
-
-
-
-
 const observer = new IntersectionObserver(function (entries) {
-
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
       entry.target.style.animation = `anim1 1s forwards ease-out`;
     } else {
-      entry.target.style.animation = 'none';
+      entry.target.style.animation = "none";
     }
   });
 });
 
-headings.forEach(heading => {
+headings.forEach((heading) => {
   observer.observe(heading);
 });
 
 //about content
 
-
 const observerContent = new IntersectionObserver(function (entries) {
-
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
-      entry.target.style.animation = `anim2 3s forwards ease-out`;
-      
+      entry.target.style.animation = `anim2 2s forwards ease-out`;
     } else {
-      entry.target.style.animation = 'none';
+      entry.target.style.animation = "none";
     }
   });
 });
 
 observerContent.observe(aboutContent);
 
-
-
-
 //end of animations
-
 
 //form validation
 let name = document.querySelector("#name");
 let email = document.querySelector("#email");
-let mobile = document.querySelector("#phoneNumber");
 let subject = document.querySelector("#subject");
 let contactForm = document.querySelector("#contactForm");
 
@@ -177,15 +153,15 @@ function printError(elemId, hintMsg) {
 }
 
 contactForm.addEventListener("submit", function (e) {
-  e.preventDefault();
+  // e.preventDefault();
   validateForm();
 });
 
 //function validate form
 function validateForm() {
-  let namErr = emailErr = mobileErr = subErr = messagErr = true;
-//name
-  if (name.value ==="") {
+  let namErr = (emailErr = subErr = messagErr = true);
+  //name
+  if (name.value === "") {
     printError("namErr", "This field is required!");
   } else {
     let regex = /^[a-zA-Z\s]+$/;
@@ -197,45 +173,35 @@ function validateForm() {
     }
   }
   //email
-  
-  if(email.value===""){
-    printError("emailErr","This field is required!");
-  }else{  
+
+  if (email.value === "") {
+    printError("emailErr", "This field is required!");
+  } else {
     printError("emailErr", "");
     emailErr = false;
   }
-  
-  //PHONENUMMBER
-  if(mobile.value !== ""){
-    let regex= /^[0-9]\d{9}$/;
-    if(regex.test(mobile.value) === false) {
-      printError("mobileErr", "Please enter a valid 10 digit mobile number");
-  }else{
-    printError("mobileErr", "");
-    mobileErr = false;
-  }
-  }
- 
-//subject
-if(subject.value=== ""){
-  printError("subErr","This field is required!");
-}else{  
-  printError("subErr", "");
-  subErr = false;
-}
-//message 
-if(message.value=== ""){
-  printError("messagErr","This field is required!");
-}else{  
-  printError("messagErr", "");
-  messagErr = false;
-}
 
+  //subject
+  if (subject.value === "") {
+    printError("subErr", "This field is required!");
+  } else {
+    printError("subErr", "");
+    subErr = false;
+  }
+  //message
+  if (message.value === "") {
+    printError("messagErr", "This field is required!");
+  } else {
+    printError("messagErr", "");
+    messagErr = false;
+  }
 
-if((namErr || emailErr || mobileErr || subErr || messagErr) === true){
-  return false;
-}else{
-  return true;
-}
+  if ((namErr || emailErr || subErr || messagErr) === true) {
+    return false;
+    window.history.back();
+  } else {
+    return true;
+    window.history.back();
+  }
   //end of validate function
 }
